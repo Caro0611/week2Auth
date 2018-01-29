40e5de21dea7ca1c11858b2991a532d699a1e2af
Mendez Acosta <a_mendezacosta@tss-hr420-dm21.fc.ca>
2018-01-29 13:31:41 -0500

    adding last part

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //just like an include or
+const app = express(); //create an instance of our aplication
+const io = require('socket.io')();
 
 app.use(express.static('public'));
 
-
+// set up routes
 app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/users'));
 
-//this goes to another path
-//app.get('/contact', (req, res) => {
-//  res.sendFile(__dirname + '/contact.html');
-//});
-
-//this goes to another path
-//app.get('/users', (req, res) => {
-//  res.sendFile(__dirname + '/users.html');
-//});
-
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log ('app running on port 3000!');
+});
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+ console.log('a user has connected!');
+ io.emit('connectMsg', { for:'everyone', msg : `${socket.id} is here!`});
+
+socket.on('disconnected', () =>{
+  console.log('a user has disconnected!');
+});
+
 });
